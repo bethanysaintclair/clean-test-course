3b0069c56e51230c8006d752094154c84fd0f2af
bethanysaintclair <bethanysaintclair@gmail.com>
2026-06-16 13:55:17 -0700

    added and automated unit tests

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -26,12 +26,12 @@ def test_MiddleOfTheRoadItems():
   assert cost == 5
 
 def test_LittleItems():
-  #Arrange
-    order = MockSet()
-    order.add(MockModel(quantity=2))
-    order.add(MockModel(quantity=2))
-    delivery_distance = 3
+#Arrange
+  order = MockSet()
+  order.add(MockModel(quantity=3))
+  order.add(MockModel(quantity=1))
+  del_dist = 2
   #Act
-    cost = Delivery.calculate(order,delivery_distance)
+  cost = Delivery.calculate(order, del_dist)
   #Assert
-    assert cost == 2.5
+  assert cost == 2.50
